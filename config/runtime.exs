f04a3efaad97af1af216eff04ed3d18988c1d2fe
Dmitry Popov <dmitriypopovsamara@gmail.com>
2025-12-23 11:27:17 +0100

    feat(core): fixed config

--- config/runtime.exs
+++ config/runtime.exs
@@ -59,7 +59,7 @@ if config_env() == :prod do
   config :wanderer_ops, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :wanderer_ops, WandererOpsWeb.Endpoint,
-    url: [host: host, port: 4000, scheme: "http"],
+    url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
